aa4b900277b76ced594eda0c44602a26bdbbb371
Ulil Albab <ulilalbabcmpk@gmail.com>
2024-10-07 04:17:20 +0700

    Delete file app.jsx and link in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App  from './App'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import LoginPage from './pages/login'
 import RegisterPage from './pages/register'
-import Hero from './components/Elements/Hero/Hero'
 import Opening from './pages/opening'
 import HomePage from './pages/homepage'
 
