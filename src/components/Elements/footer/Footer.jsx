b6623ebd3110c19c9c43998f7bd6a93d62808dbe
Ulil Albab <ulilalbabcmpk@gmail.com>
2024-10-07 04:29:08 +0700

    fixed footer

--- src/components/Elements/footer/Footer.jsx
+++ src/components/Elements/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             </div>
             <div className="flex flex-col justify-center">
                 <h4 className="font-bold">Genre</h4>
-                <img className="lg:hidden sm:block absolute right-5 bottom-[-151.5%]" src="../assets/arrow-footer.png" alt="" />
+                <img className="lg:hidden sm:block absolute right-5 bottom-[-155%]" src="../assets/arrow-footer.png" alt="" />
                 <div className="flex justify-center gap-5">
                     <ul className="list-none hidden lg:block">
                         <li className="py-2"><a href="#" className="no-underline hover:underline">Aksi</a></li>
@@ -35,7 +35,7 @@ const Footer = () => {
             </div>
             <div className="mr-10">
                 <h4 className="font-bold">Bantuan</h4>
-                <img src="../assets/arrow-footer.png" alt="" className="lg:hidden sm:block absolute bottom-[-157.5%] right-5 "/>
+                <img src="../assets/arrow-footer.png" alt="" className="lg:hidden sm:block absolute bottom-[-160.5%] right-5 "/>
                 <ul className=" list-none hidden lg:block">
                     <li className="py-2"><a href="#" className="no-underline hover:underline">FAQ</a></li>
                     <li className="py-2"><a href="#" className="no-underline hover:underline">Kontak Kami</a></li>
